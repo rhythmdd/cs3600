1e68eb91ae27b393337b7b211044f462aeeea1a3
rhythmdd <rdrhythm645@gmail.com>
2015-09-10 01:22:32 +0530

    Update houseassn2.cpp

--- houseassn2.cpp
+++ houseassn2.cpp
@@ -1,27 +1,3 @@
-/* Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- * 
- * The above notice and this permission notice shall be included in all copies
- * or substantial portions of the Software.
- * 
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- */
-/* File for "Lighting" lesson of the OpenGL tutorial on
- * www.videotutorialsrock.com
- */
-
-
-
 #include <iostream>
 #include <stdlib.h>
 
@@ -31,8 +7,12 @@
 #else
 #include <GL/glut.h>
 #endif
-
+//#include<SDL/SDL.h>
+#define checkImageHeight 30
+#define checkImageWidth 30
 using namespace std;
+
+static GLubyte checkImage[checkImageHeight][checkImageWidth][4];
 
 //Called when a key is pressed
 void handleKeypress(unsigned char key, int x, int y) {
@@ -41,29 +21,91 @@ void handleKeypress(unsigned char key, int x, int y) {
 			exit(0);
 	}
 }
-
-//Initializes 3D rendering
+GLuint texName;
 void initRendering() {
 	glEnable(GL_DEPTH_TEST);
 	glEnable(GL_COLOR_MATERIAL);
-	glEnable(GL_LIGHTING); //Enable lighting
-	glEnable(GL_LIGHT0); //Enable light #0
-	glEnable(GL_LIGHT1); //Enable light #1
-	glEnable(GL_NORMALIZE); //Automatically normalize normals
-	//glShadeModel(GL_SMOOTH); //Enable smooth shading
+	glEnable(GL_LIGHTING); 
+	glEnable(GL_LIGHT0); 
+	glEnable(GL_LIGHT1);
+	glEnable(GL_NORMALIZE);
 }
 
-//Called when the window is resized
 void handleResize(int w, int h) {
-	glViewport(0, 0, w, h);
+	glViewport(0, 0,(GLsizei) w,(GLsizei) h);
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
 	gluPerspective(60.0, (double)w / (double)h, 1.0, 200.0);
 }
 
 float _angle = -70.0f;
+unsigned int tex;
 
-//Draws the 3D scene
+void loadtex(void){
+	int i,j,c;
+	for (i = 0; i < checkImageHeight; i++) {
+      for (j = 0; j < checkImageWidth; j++) {
+         c = ((((i&0x8)==0)^((j&0x8))==0))*255;
+         checkImage[i][j][0] = (GLubyte) c;
+         checkImage[i][j][1] = (GLubyte) c;
+         checkImage[i][j][2] = (GLubyte) c;
+         checkImage[i][j][3] = (GLubyte) 255;
+      }
+   }
+	
+}
+/*void loadTexture(void){
+	//SDL_Surface* img=SDL_LoadBMP(filename);
+	unsigned int id;
+	glGenTextures(1,&id);
+	glBindTexture(GL_TEXTURE_2D,id);
+	//glTexImage(GL_TEXTURE_2D,0,GL_RGB,img->w,img->h,0,GL_RGB,GL_UNSIGNED_SHORT_5_6_5,img->pixels);
+	glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAX_FILTER,GL_LINEAR);
+	//SDL_FreeSurface(img);
+	//return id;
+}*/
+void init(void)
+{    
+   glClearColor (0.0, 0.0, 0.0, 0.0);
+   glShadeModel(GL_FLAT);
+   glEnable(GL_DEPTH_TEST);
+    
+
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    
+    GLfloat lightpos[] = { 0.0, 15.0, 15.0 };
+    GLfloat lightcolor[] = { 0.5, 0.5, 0.5 };
+    GLfloat ambcolor[] = { 0.2, 0.2, 0.0 };
+    
+    glEnable(GL_LIGHTING);
+    glLightModelfv(GL_LIGHT_MODEL_AMBIENT,ambcolor);
+    
+    glEnable(GL_LIGHT0);
+    glLightfv(GL_LIGHT0,GL_POSITION,lightpos);
+    glLightfv(GL_LIGHT0,GL_AMBIENT,lightcolor);
+    glLightfv(GL_LIGHT0,GL_DIFFUSE,lightcolor);
+    glLightfv(GL_LIGHT0,GL_SPECULAR,lightcolor);
+
+
+   loadtex();
+   glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+
+   glGenTextures(1, &texName);
+   glBindTexture(GL_TEXTURE_2D, texName);
+
+   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, 
+                   GL_NEAREST);
+   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, 
+                   GL_NEAREST);
+   glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, checkImageWidth, 
+                checkImageHeight, 0, GL_RGBA, GL_UNSIGNED_BYTE, 
+                checkImage);
+}
+
 void drawScene() {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	
@@ -72,19 +114,16 @@ void drawScene() {
 	
 	glTranslatef(0.0f, 0.0f, -8.0f);
 	
-	//Add ambient light
-	GLfloat ambientColor[] = {0.2f, 0.2f, 0.2f, 1.0f}; //Color (0.2, 0.2, 0.2)
+	GLfloat ambientColor[] = {0.2f, 0.2f, 0.2f, 1.0f};
 	glLightModelfv(GL_LIGHT_MODEL_AMBIENT, ambientColor);
 	
-	//Add positioned light
-	GLfloat lightColor0[] = {0.5f, 0.5f, 0.5f, 1.0f}; //Color (0.5, 0.5, 0.5)
-	GLfloat lightPos0[] = {4.0f, 0.0f, 8.0f, 1.0f}; //Positioned at (4, 0, 8)
+	GLfloat lightColor0[] = {0.5f, 0.5f, 0.5f, 1.0f};
+	GLfloat lightPos0[] = {4.0f, 0.0f, 8.0f, 1.0f};
 	glLightfv(GL_LIGHT0, GL_DIFFUSE, lightColor0);
 	glLightfv(GL_LIGHT0, GL_POSITION, lightPos0);
 	
-	//Add directed light
-	GLfloat lightColor1[] = {0.5f, 0.2f, 0.2f, 1.0f}; //Color (0.5, 0.2, 0.2)
-	//Coming from the direction (-1, 0.5, 0.5)
+	GLfloat lightColor1[] = {0.5f, 0.2f, 0.2f, 1.0f};
+
 	GLfloat lightPos1[] = {-1.0f, 0.5f, 0.5f, 0.0f};
 	glLightfv(GL_LIGHT1, GL_DIFFUSE, lightColor1);
 	glLightfv(GL_LIGHT1, GL_POSITION, lightPos1);
@@ -94,123 +133,27 @@ void drawScene() {
 	
 	
 	
-	/*
-	
-	glClear(GL_COLOR_BUFFER_BIT);     // Clear display window
-    // Set line segment color as glColor3f(R,G,B)
-    glColor3f(0.3, 0.5, 0.8);
-    glBegin(GL_POLYGON);
-    glVertex2i(2, 5);
-    glVertex2i(6, 5);
-    glVertex2f(7.0f, 3.5f);
-    glVertex2f(3.0f, 3.50f);
-    glEnd();
-    // Top of Front Wall
-    glColor3f(0.1, 0.5, 0.0);
-    glBegin(GL_TRIANGLES);
-    glVertex2i(2, 5);
-    glVertex2f(1.0, 3.5);
-    glVertex2f(3.0, 3.5);
-    glEnd();
-    // Front Wall
-    glColor3f(0.7, 0.2, 0.3);
-    glBegin(GL_POLYGON);
-    glVertex2f(1.0, 3.5);
-    glVertex2f(3.0, 3.5);
-    glVertex2i(3, 1);
-    glVertex2i(1, 1);
-    glEnd();
-    // Front Door
-    glColor3f(0.7, 0.2, 0.9);
-    glBegin(GL_POLYGON);
-    glVertex2f(1.50, 2.50);
-    glVertex2f(2.50, 2.50);
-    glVertex2f(2.50, 1.00);
-    glVertex2f(1.50, 1.00);
-    glEnd();
- 
-    // Front Door Lock
-    glColor3f(0.3, 0.7, 0.9);
-    glPointSize(0.15);
-    glBegin(GL_POINTS);
-    glVertex2f(1.70, 1.70);
-    glEnd();
- 
-    //side Wall
-    glColor3f(0.1, 0.2, 0.3);
-    glBegin(GL_POLYGON);
-    glVertex2f(3.00, 3.50);
-    glVertex2f(7.00, 3.50);
-    glVertex2f(7.00, 1.00);
-    glVertex2f(3.00, 1.00);
-    glEnd();
-    // window one
-    glColor3f(0.2, 0.4, 0.3);
-    glBegin(GL_POLYGON);
-    glVertex2f(3.30, 3.20);
-    glVertex2f(4.50, 3.20);
-    glVertex2f(4.50, 2.30);
-    glVertex2f(3.30, 2.30);
-    glEnd();
-    // line of window one
-    glColor3f(0.1, 0.7, 0.5);
-    glLineWidth(0.05);
-    glBegin(GL_LINES);
-    glVertex2f(3.90, 3.20);
-    glVertex2f(3.90, 2.30);
-    glVertex2f(3.30, 2.73);
-    glVertex2f(4.50, 2.73);
-    glEnd();
-    // window two
-    glColor3f(0.2, 0.4, 0.3);
-    glBegin(GL_POLYGON);
-    glVertex2f(5.30, 3.20);
-    glVertex2f(6.50, 3.20);
-    glVertex2f(6.50, 2.30);
-    glVertex2f(5.30, 2.30);
-    glEnd();
-    // lines of window two
-    glColor3f(0.1, 0.7, 0.5);
-    glLineWidth(0.05);
-    glBegin(GL_LINES);
-    glVertex2f(5.90, 3.20);
-    glVertex2f(5.90, 2.30);
-    glVertex2f(5.30, 2.73);
-    glVertex2f(6.50, 2.73);
-    glEnd();
- 
-    // Entrance Path
-    glColor3f(0.3, 0.5, 0.7);
-    glLineWidth(.03);
-    glBegin(GL_POLYGON);
-    glVertex2f(1.50, 1.00);
-    glVertex2f(2.50, 1.00);
-    glVertex2f(2.10, .0);
-    glVertex2f(.40, .0);
-    glEnd();
-    // Process all OpenGL routine s as quickly as possible
-    glFlush();
-	
-	*/
-	
 	
 	
 	
 	glColor3f(1.0f, 1.0f, 0.0f);
 	glBegin(GL_QUADS);
 	
+	glEnable(GL_TEXTURE_2D);
+	glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_DECAL);
+    glBindTexture(GL_TEXTURE_2D, texName);
 	//Front
 	glNormal3f(0.0f, 0.0f, 1.0f);
-	//glNormal3f(-1.0f, 0.0f, 1.0f);
+	glTexCoord2f(0.0,0.0);
 	glVertex3f(-1.5f, -1.0f, 1.5f);
-	//glNormal3f(1.0f, 0.0f, 1.0f);
+	glTexCoord2f(1.0,0.0);
 	glVertex3f(1.5f, -1.0f, 1.5f);
-	//glNormal3f(1.0f, 0.0f, 1.0f);
+	glTexCoord2f(1.0,1.0);
 	glVertex3f(1.5f, 1.0f, 1.5f);
-	//glNormal3f(-1.0f, 0.0f, 1.0f);
+	glTexCoord2f(0.0,1.0);
 	glVertex3f(-1.5f, 1.0f, 1.5f);
 	
-	
+	glDisable(GL_TEXTURE_2D);
 	//top right
 	//glNormal3f(1.0f, 0.0f, 0.0f);
 	//glNormal3f(1.0f, 0.0f, -1.0f);
@@ -224,94 +167,90 @@ void drawScene() {
 	glVertex3f(0.0f, 1.5f, 1.5f);
 	
 	//top left
-	//glNormal3f(1.0f, 0.0f, 0.0f);
-	//glNormal3f(1.0f, 0.0f, -1.0f);
 	glVertex3f(-1.5f, 1.0f, 1.5f);
-	//glNormal3f(1.0f, 0.0f, -1.0f);
 	glVertex3f(-1.5f, 1.0f, -1.5f);
-	//glNormal3f(1.0f, 0.0f, 1.0f);
 	glVertex3f(0.0f, 1.5f, -1.5f);
-	//glNormal3f(1.0f, 0.0f, 1.0f);
 	glVertex3f(0.0f, 1.5f, 1.5f);
 	
 	glColor3f(.0f, .0f, 1.0f);
 	
 	//door offset
 	glVertex3f(1.55f, 0.5f, 0.5f);
-	//glNormal3f(1.0f, 0.0f, -1.0f);
 	glVertex3f(1.5f, 0.5f, 0.5f);
-	//glNormal3f(1.0f, 0.0f, 1.0f);
 	glVertex3f(1.5f, -1.0f, 0.5f);
-	//glNormal3f(1.0f, 0.0f, 1.0f);
 	glVertex3f(1.55f, -1.0f, 0.5f);
 	
 	//door offset
 	glVertex3f(1.55f, 0.5f, -0.5f);
-	//glNormal3f(1.0f, 0.0f, -1.0f);
 	glVertex3f(1.5f, 0.5f, -0.5f);
-	//glNormal3f(1.0f, 0.0f, 1.0f);
 	glVertex3f(1.5f, -1.0f, -0.5f);
-	//glNormal3f(1.0f, 0.0f, 1.0f);
 	glVertex3f(1.55f, -1.0f, -0.5f);
 	
 	//door offset
 	glVertex3f(1.55f, 0.5f, 0.5f);
-	//glNormal3f(1.0f, 0.0f, -1.0f);
 	glVertex3f(1.5f, 0.5f, 0.5f);
-	//glNormal3f(1.0f, 0.0f, 1.0f);
 	glVertex3f(1.5f, 0.5f, -0.5f);
-	//glNormal3f(1.0f, 0.0f, 1.0f);
 	glVertex3f(1.55f, 0.5f, -0.5f);
 	
 	//door
-	//glNormal3f(1.0f, 0.0f, 0.0f);
-	//glNormal3f(1.0f, 0.0f, -1.0f);
 	glColor3f(0.0f, 1.0f, 0.0f);
 	
 	
 	glVertex3f(1.55f, -1.0f, -.5f);
-	//glNormal3f(1.0f, 0.0f, -1.0f);
 	glVertex3f(1.55f, 0.5f, -.5f);
-	//glNormal3f(1.0f, 0.0f, 1.0f);
 	glVertex3f(1.55f, .5f, .5f);
-	//glNormal3f(1.0f, 0.0f, 1.0f);
 	glVertex3f(1.55f, -1.0f, .5f);
 	
+	glEnd();
+	
+	
+	//front covering
+	glColor3f(0.5f,0.7,0.9);
+	glBegin(GL_TRIANGLES);
+	glVertex3f(-1.5,1.0,1.5);
+	glVertex3f(0.0,1.5,1.5);
+	glVertex3f(1.5,1.0,1.5);
+	glEnd();
+	
+	glBegin(GL_TRIANGLES);
+	glVertex3f(-1.5,1.0,-1.5);
+	glVertex3f(0.0,1.5,-1.5);
+	glVertex3f(1.5,1.0,-1.5);
+	
+	glEnd();
+	
+	
+	glBegin(GL_QUADS);
 		glColor3f(1.0f, 1.0f, 0.0f);
 
+	glEnable(GL_TEXTURE_2D);
+	glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_DECAL);
+    glBindTexture(GL_TEXTURE_2D, texName);
 	//Right
 	glNormal3f(1.0f, 0.0f, 0.0f);
-	//glNormal3f(1.0f, 0.0f, -1.0f);
+	glTexCoord2f(0.0,0.0);
 	glVertex3f(1.5f, -1.0f, -1.5f);
-	//glNormal3f(1.0f, 0.0f, -1.0f);
+	glTexCoord2f(0.0,1.0);
 	glVertex3f(1.5f, 1.0f, -1.5f);
-	//glNormal3f(1.0f, 0.0f, 1.0f);
+	glTexCoord2f(1.0,1.0);
 	glVertex3f(1.5f, 1.0f, 1.5f);
-	//glNormal3f(1.0f, 0.0f, 1.0f);
+	glTexCoord2f(1.0,0.0);
 	glVertex3f(1.5f, -1.0f, 1.5f);
 	
 	//Back
 	glNormal3f(0.0f, 0.0f, -1.0f);
-	//glNormal3f(-1.0f, 0.0f, -1.0f);
 	glVertex3f(-1.5f, -1.0f, -1.5f);
-	//glNormal3f(-1.0f, 0.0f, -1.0f);
 	glVertex3f(-1.5f, 1.0f, -1.5f);
-	//glNormal3f(1.0f, 0.0f, -1.0f);
 	glVertex3f(1.5f, 1.0f, -1.5f);
-	//glNormal3f(1.0f, 0.0f, -1.0f);
 	glVertex3f(1.5f, -1.0f, -1.5f);
 	
 	//Left
 	glNormal3f(-1.0f, 0.0f, 0.0f);
-	//glNormal3f(-1.0f, 0.0f, -1.0f);
 	glVertex3f(-1.5f, -1.0f, -1.5f);
-	//glNormal3f(-1.0f, 0.0f, 1.0f);
 	glVertex3f(-1.5f, -1.0f, 1.5f);
-	//glNormal3f(-1.0f, 0.0f, 1.0f);
 	glVertex3f(-1.5f, 1.0f, 1.5f);
-	//glNormal3f(-1.0f, 0.0f, -1.0f);
 	glVertex3f(-1.5f, 1.0f, -1.5f);
-	
+	glDisable(GL_TEXTURE_2D);
 	glEnd();
 	
 	glutSwapBuffers();
@@ -336,7 +275,7 @@ int main(int argc, char** argv) {
 	//Create the window
 	glutCreateWindow("Lighting - videotutorialsrock.com");
 	initRendering();
-	
+	//init();
 	//Set handler functions
 	glutDisplayFunc(drawScene);
 	glutKeyboardFunc(handleKeypress);
@@ -347,3 +286,4 @@ int main(int argc, char** argv) {
 	glutMainLoop();
 	return 0;
 }
+
